6aab3ba15df291c8133c88c38c3fdf50ac5f8fcb
mberlin1 <mberlin1@umbc.edu>
2016-03-05 13:45:09 -0500

    Very simple java listener/client
    Opens a socket to 42069, for the IP request I was using localhost but you can use any you wish.  We can put the listener into a loop as necessary

---
+++ ExpressibotListener.java
@@ -0,0 +1,40 @@
+//original code from http://cs.lmu.edu/~ray/notes/javanetexamples/
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+
+public class ExpressibotListener {
+
+	/**
+	 * A TCP server that runs on port 9090.  When a client connects, it
+	 * sends the client the current date and time, then closes the
+	 * connection with that client.  Arguably just about the simplest
+	 * server you can write.
+	 */
+
+	/**
+	 * Runs the server.
+	 */
+	public static void main(String[] args) throws IOException {
+		ServerSocket listener = new ServerSocket(42069);
+		try {
+			while (true) {
+				Socket socket = listener.accept();
+				try {
+					
+					PrintWriter out =
+							new PrintWriter(socket.getOutputStream(), true);
+					out.println(new Date().toString());
+				} finally {
+					socket.close();
+				}
+			}
+		}
+		finally {
+			listener.close();
+		}
+	}
+}
